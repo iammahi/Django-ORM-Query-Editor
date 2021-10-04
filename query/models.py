c0a4531aedd8dbbb1ffabc397205ad63b170daf8
iammahi <iammahi1122@gmail.com>
2021-10-04 13:09:20 +0530

    first

---
+++ query/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Deptno(models.Model):
+	deptno = models.IntegerField(primary_key=True)
+	dname  = models.CharField(max_length=30)
+	loc    = models.CharField(max_length=20)
+class Emp(models.Model):
+	empno = models.IntegerField(primary_key=True,unique=True)
+	ename = models.CharField( max_length=50)
+	job = models.CharField( max_length=50)
+	manager = models.CharField( blank=True,max_length=50,null=True)
+	hiredate = models.DateField(null=True)
+	sal      =models.IntegerField()
+	cmm = models.IntegerField(blank=True,null=True)
+	deptno = models.ForeignKey('Deptno',on_delete=models.CASCADE)
---
+++ query/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+import simplejson as json
+from django.db import models
+from django.db.models import *
+from django.db.models.functions import *
+from django.core.exceptions import *
+
+from django.forms.models import model_to_dict # is used to convert the single record into dic
+# Create your views here.
+def Home(request):
+	emp = Emp.objects.all().count()
+	dep = Deptno.objects.all().count()
+	
+	
+	d=[{'Table':'Deptno' ,'count':dep,'q':'Deptno.objects.all()'},{'Table':'Emp' ,'count':emp,'q':'Emp.objects.all()'}]
+	return  render(request,'index.html',{'d':d})
+
+def Ajax(request):
+	try:
+		name = request.GET['name']
+		l=[]
+		if 'values'  in name:
+			for i in eval(name):
+				i['hiredate']=str(i['hiredate'])
+				l.append(i)
+				
+			return HttpResponse(json.dumps(l))
+		elif '.annotate' in name:
+			for i in eval(name).values():
+				i['hiredate']=str(i['hiredate'])
+				l.append(i)
+			return HttpResponse(json.dumps(l))
+		elif '.aggregate(' in name:
+			l=eval(name)
+			l['hiredate']=str(l['hiredate'])
+			l=[l]
+			return HttpResponse(json.dumps(l))
+		elif '.count()' in name:
+			d ={'count':eval(name)}
+			l=[d]
+			return HttpResponse(json.dumps(l))
+		elif '.update(' in name:
+			a=eval(name)
+			a.save()
+			return HttpResponse('<h1>Updated</h1>')
+			
+		else:
+			for i in eval(name):
+				s=model_to_dict(i)
+				if 'hiredate' in s:
+					s['hiredate']=str(s['hiredate'])
+				l.append(s)
+			return HttpResponse(json.dumps(l))
+	except Exception as ex:
+		print(ex)
+		return HttpResponse(str(ex)+str(name))
+		
+			
